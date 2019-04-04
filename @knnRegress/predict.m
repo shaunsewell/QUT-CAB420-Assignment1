a415deeba14b1efbf52204355116cc5376f1b473
Shaun Sewell <shaun.sewell91+github@gmail.com>
2019-04-04 11:20:07 +1000

    Update predict.m

--- @knnRegress/predict.m
+++ @knnRegress/predict.m
@@ -8,11 +8,11 @@
       for i=1:Nte,                          % For each test example:
         dist = sum( bsxfun( @minus, obj.Xtrain, Xte(i,:) ).^2 , 2);  % compute sum of squared differences
         %dist = sum( (obj.Xtrain - repmat(Xte(i,:),[Ntr,1]) ).^2 , 2);  % compute sum of squared differences
+        
         [tmp,idx] = sort(dist);              % find nearest neighbors over Xtrain (dimension 2)
                                             % idx(1) is the index of the nearest point, etc.; see help sort
 
-        error('You should write prediction code here');
-        Yte(i)=something_you_write();       % predict ith test example's value from nearest neighbors
+        Yte(i)=mean(obj.Ytrain(idx(1:K)));       % predict ith test example's value from nearest neighbors
 
-      end;
+      end
     end
